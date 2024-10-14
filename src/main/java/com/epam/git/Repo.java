b60a10ef07773dfa8147c74cdd1dc70e41aef056
n2749 <nurbolat.dav@gmail.com>
2024-10-14 21:02:35 +0500

    finished task

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/N2749/mjc_module2_task3_git";
 }
